166930d4bf67d85fbed81eb2756257e60a81ea59
Evgeniy <ifzy12@gmail.com>
2023-10-15 18:12:14 +0300

    add crud user implementation

---
+++ src/main/java/academy/kata/dao/UserDao.java
@@ -0,0 +1,14 @@
+package academy.kata.dao;
+
+import academy.kata.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    void save(User user);
+    List<User> findAll();
+    Optional<User> findById(Long id);
+    void updateUser(User user);
+    void deleteById(Long id);
+}
